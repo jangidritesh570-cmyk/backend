592bd69dfc8f67db8117f0c1e8635ad41ab53349
jangidritesh570-cmyk <jangidritesh570@gmail.com>
2026-06-18 23:17:00 +0530

    sendEmail updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,23 +4,32 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
 });
 
 export const sendEmail = async (message) => {
   try {
-    await transporter.sendMail({
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+
+    await transporter.verify();
+    console.log("SMTP READY");
+
+    const info = await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: "jangidritesh570@gmail.com",
       subject: "❤️ Bhoomi Accepted Your Sorry",
       text: message,
     });
 
-    console.log("EMAIL SENT");
+    console.log("EMAIL SENT:", info.messageId);
   } catch (error) {
     console.log("EMAIL ERROR:", error);
     throw error;
